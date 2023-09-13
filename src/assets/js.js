038b00b303074950c3b321a2eb0debbe33bc7bfe
zeiadsalhin <zeyadali011@gmail.com>
2023-09-13 09:31:38 +0300

    v1

--- src/assets/js.js
+++ src/assets/js.js
@@ -16,42 +16,42 @@ const water = document.querySelector("#water")
 const custom = document.querySelector("#custom")
 
 
-// b1.addEventListener('click', () => {
-//   setTimeout(() => {
-//     if (b1.innerHTML === 'ايقاف') {
-//       b1.innerHTML = ('تشغيل')
-//       b1.classList.remove("bg-red-700")
-//       b1.classList.toggle("bg-green-700")
-//       light.innerHTML = ('متوقف')
-//       light.classList.add('text-red-700')
-//     } else if (b1.innerHTML === 'تشغيل') {
-//       b1.innerHTML = ('ايقاف')
-//       b1.classList.remove("bg-green-700")
-//       b1.classList.toggle("bg-red-700")
-//       light.innerHTML = ('مفعل')
-//       light.classList.remove('text-red-700')
-//       light.classList.add('text-green-600')
-//     }
-//   }, 1000);
-// });
-// b2.addEventListener('click', () => {
-//   setTimeout(() => {
-//     if (b2.innerHTML === 'ايقاف') {
-//       b2.innerHTML = ('تشغيل')
-//       b2.classList.remove("bg-red-700")
-//       b2.classList.toggle("bg-green-700")
-//       water.innerHTML = ('متوقف')
-//       water.classList.add('text-red-700')
-//     } else if (b2.innerHTML === 'تشغيل') {
-//       b2.innerHTML = ('ايقاف')
-//       b2.classList.remove("bg-green-700")
-//       b2.classList.toggle("bg-red-700")
-//       water.innerHTML = ('مفعل')
-//       water.classList.remove('text-red-700')
-//       water.classList.add('text-green-600')
-//     }
-//   }, 1000);
-// });
+b1.addEventListener('click', () => {
+  setTimeout(() => {
+    if (b1.innerHTML === 'ايقاف') {
+      b1.innerHTML = ('تشغيل')
+      b1.classList.remove("bg-red-700")
+      b1.classList.toggle("bg-green-700")
+      light.innerHTML = ('متوقف')
+      light.classList.add('text-red-700')
+    } else if (b1.innerHTML === 'تشغيل') {
+      b1.innerHTML = ('ايقاف')
+      b1.classList.remove("bg-green-700")
+      b1.classList.toggle("bg-red-700")
+      light.innerHTML = ('مفعل')
+      light.classList.remove('text-red-700')
+      light.classList.add('text-green-600')
+    }
+  }, 1000);
+});
+b2.addEventListener('click', () => {
+  setTimeout(() => {
+    if (b2.innerHTML === 'ايقاف') {
+      b2.innerHTML = ('تشغيل')
+      b2.classList.remove("bg-red-700")
+      b2.classList.toggle("bg-green-700")
+      water.innerHTML = ('متوقف')
+      water.classList.add('text-red-700')
+    } else if (b2.innerHTML === 'تشغيل') {
+      b2.innerHTML = ('ايقاف')
+      b2.classList.remove("bg-green-700")
+      b2.classList.toggle("bg-red-700")
+      water.innerHTML = ('مفعل')
+      water.classList.remove('text-red-700')
+      water.classList.add('text-green-600')
+    }
+  }, 1000);
+});
 function customf() {
   if (custom.innerHTML === 'تلقائي') {
     custom.innerHTML = ('مخصص')
