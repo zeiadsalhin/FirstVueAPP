bf97bcd6f8c117570cc5d328111dfb62c9fcee4d
zeiadsalhin <zeyadali011@gmail.com>
2023-09-13 06:11:19 +0300

    v1

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import '../src/assets/index.css'
+import './assets/index.css'
 
 const app = createApp(App)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+
 
 <template>
   <main>
-    <TheWelcome />
+    <div>
+      <h1 class="text-white p-2">Hello</h1>
+    </div>
   </main>
 </template>
