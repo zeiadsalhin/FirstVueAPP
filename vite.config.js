243fee7520e3d22bb8da14467f158759694ebab0
zeiadsalhin <zeyadali011@gmail.com>
2023-09-18 12:26:13 +0300

    v1

--- vite.config.js
+++ vite.config.js
@@ -2,40 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({
-      // generates 'manifest.webmanifest' file on build
-      manifest: {
-        // caches the assets/icons mentioned (assets/* includes all the assets present in your src/ directory) 
-        includeAssets: ["favicon.ico", "apple-touch-icon.png", "assets/*"],
-        name: 'Simplifying Progressive Web App (PWA) Development with Vite: A Beginners Guide',
-        short_name: 'PWA Guide',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: '/public/img/icon/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/public/img/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      workbox: {
-        // defining cached files formats
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
-      }
-    }),
 
   ],
   resolve: {
