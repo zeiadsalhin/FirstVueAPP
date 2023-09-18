8a0a11768ef05a1d66939f4a6b5c699d57139e9e
zeiadsalhin <zeyadali011@gmail.com>
2023-09-18 14:30:10 +0300

    v1

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,44 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      injectRegister: 'auto',
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "logo.svg"],
+      manifest: {
+        name: "Farm DashBoard",
+        short_name: "Short Name",
+        description: "Description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/img/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
